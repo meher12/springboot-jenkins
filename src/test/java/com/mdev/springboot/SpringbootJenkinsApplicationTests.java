1b7cd8bbb8af3ab0656c0303be711f338c920734
meher12 <meher.khemiri@gmail.com>
2022-02-17 10:17:58 +0100

    second commit

--- src/test/java/com/mdev/springboot/SpringbootJenkinsApplicationTests.java
+++ src/test/java/com/mdev/springboot/SpringbootJenkinsApplicationTests.java
@@ -16,6 +16,7 @@ class SpringbootJenkinsApplicationTests {
 	void contextLoads() {
 
 		logger.info("Test case executing..");
+		logger.info("Test case executing second log statement");
 		assertEquals(true, true);
 	}
 
